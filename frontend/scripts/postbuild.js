cf8493a044e1af6bc15de0ce5e10a8e8ad5cb49b
aliyousaff <87736676+aliyousaff@users.noreply.github.com>
2025-07-29 12:15:17 +0500

    Revert postbuild.js to rename index.html for SSR

--- frontend/scripts/postbuild.js
+++ frontend/scripts/postbuild.js
@@ -4,5 +4,5 @@ import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 console.log(`Running frontend post build script...`);
-// renameSync(path.resolve(__dirname, '../dist/index.html'), path.resolve(__dirname, '../dist/_index.html'));
-// console.log(`index.html renamed to _index.html so SSR of the index page can be assumed.`);
+renameSync(path.resolve(__dirname, '../dist/index.html'), path.resolve(__dirname, '../dist/_index.html'));
+console.log(`index.html renamed to _index.html so SSR of the index page can be assumed.`);
